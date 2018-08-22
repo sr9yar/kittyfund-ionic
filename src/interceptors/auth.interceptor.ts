7006cfa76371da869b0fd679389ced8e55065a38
Yaroslav <340999@gmail.com>
2018-08-22 12:25:26 +0300

    migration to the new version in progress

--- src/interceptors/auth.interceptor.ts
+++ src/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import {
   HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { UserService } from '../services/user/user.service';
 
 @Injectable()
